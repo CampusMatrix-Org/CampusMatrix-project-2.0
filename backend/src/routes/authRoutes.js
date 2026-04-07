97e1b8ba9bda7878b015d4a67420c6cafaf595f5
Hasitha <hasithadilshanjayakody678@gmail.com>
2026-04-07 14:21:55 +0530

    add role protected routes for Admin and Student validation

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,13 +1,22 @@
 import express from 'express';
-import { registerUser, loginUser, getMe } from '../controllers/authController.js';
-import { protect } from '../middlewares/authMiddleware.js';
+import {
+  registerUser,
+  loginUser,
+  getMe,
+  adminOnly,
+  studentOnly
+} from '../controllers/authController.js';
+import { protect, authorizeRoles } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
-// Protected test route
 router.get('/me', protect, getMe);
+
+// Role-protected test routes
+router.get('/admin-test', protect, authorizeRoles('Admin'), adminOnly);
+router.get('/student-test', protect, authorizeRoles('Student', 'Admin'), studentOnly);
 
 export default router;
